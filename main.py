8ef6cf79fff2da425bc6741397d77f976317f9a9
niggiover9000 <nicolasbartels@web.de>
2021-06-09 12:18:30 +0200

    Added sleep times

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import os
 from os.path import expanduser, isdir
 from distutils.dir_util import copy_tree
 from shutil import copyfile
+from time import sleep
 
 mods_path = os.path.join(expanduser("~"), "Documents\My Games\Sid Meier's Civilization 5\MODS")
 
 
 def gen_xml():
+    """
+    Creates the XML File used for the multiplayer mods
+    :return: XML formatted string containing the downloaded mods
+    """
     ret_xml = "\n\t<!-- MULTIPLAYER MOD FILES -->\n\n"
 
     for _, _, files in os.walk(mods_path):
@@ -19,7 +24,10 @@ def gen_xml():
     return ret_xml
 
 
-def remove_mods():
+def remove_mods(data):
+    """
+    :return:
+    """
     mod_begin = 0
     mod_end = 0
 
@@ -33,60 +41,99 @@ def remove_mods():
     del (data[mod_begin - 1:mod_end + 2])
 
 
-if __name__ == '__main__':
+def get_game_path():
+    steam_path = "\steam\steamapps\common\Sid Meier's Civilization V\Assets\DLC\Expansion2"
+    if isdir(f"C:\Program Files (x86){steam_path}"):
+        return f"C:\Program Files (x86){steam_path}"
+    else:
+        for i in range(65, 91):
+            if isdir(f"{chr(i)}:\{steam_path}"):
+                return f"{chr(i)}:\{steam_path}"
+        return False
 
-    # check for mods folder
+
+def get_mod_folder():
     if isdir(mods_path):
-        print("mods folder found.\n")
+        print("Mods folder found.\n")
     else:
-        print("mods folder not present at: " + mods_path)
+        print(f"Mods folder not present at: {mods_path}. Did you download the mods from the Steam Workshop?")
+        print("Program will terminate now.")
+        sleep(5)
         exit(-1)
 
-    # get steam install drive
-    steam_drive = input("steam install path? i.e. C:/Program Files (x86)/Steam, E:/Steam ...\n\n")
-    steam_path = os.path.join(steam_drive, "steamapps\common\Sid Meier's Civilization V\Assets\DLC\Expansion2")
 
-    # check for civ install folder
-    if isdir(steam_path):
-        print("civ folder found.\n")
-    else:
-        print("civ folder not present at: " + steam_path)
-        exit(-1)
-
-    # list subscribed mods
+def list_mods():
     mods = os.listdir(mods_path)
-    print("installing mods:")
-    print('\n'.join(mods))
+    print("Installing Mods:")
+    print('\n Installing '.join(mods))
+    return mods
 
-    # copy mods
+
+def copy_mods(mods):
     for mod in mods:
         src = os.path.join(mods_path, mod)
         target = os.path.join(steam_path, "[MOD] " + mod)
         copy_tree(src, target)
 
-    # saving pkg file
-    print()
-    print("backing up pkg file")
+
+def backup_pkg_file():
+    print("\n Backing up Expansion2.Civ5Pkg File...")
     pkg = os.path.join(steam_path, "Expansion2.Civ5Pkg")
     copyfile(pkg, pkg + ".old")
     del pkg
 
-    data = str()
 
-    # writing to pkg file
+def write_pkg_file():
     with open(os.path.join(steam_path, "Expansion2.Civ5Pkg")) as f:
         data = f.readlines()
 
-        remove_mods()
+        remove_mods(data)
 
         for i in range(len(data)):
             if "<Gameplay>\n" in data[i]:
                 xml = gen_xml()
                 data.insert(i + 1, xml)
                 break
-
     with open(os.path.join(steam_path, "Expansion2.Civ5Pkg"), "w") as f:
         f.writelines(data)
         f.flush()
 
-    print("install successful")
+if __name__ == '__main__':
+    # check for mods folder
+    get_mod_folder()
+
+    # get steam install drive
+    game_path = get_game_path()
+    if game_path is not False:
+        steam_path = game_path
+        print(f"Civ_Folder automatically detected: {steam_path}")
+    else:
+        print("Civilization V folder couldn't be detected automatically.")
+        while True:
+            steam_drive = input("Please enter your install path. i.e. C:/Program Files (x86)/Steam, E:/Steam ...: ")
+            steam_path = os.path.join(steam_drive, "steamapps\common\Sid Meier's Civilization V\Assets\DLC\Expansion2")
+            # check for civ install folder
+            if steam_drive == "/exit":
+                print("Program will terminate now.")
+                sleep(5)
+                exit(-1)
+            if isdir(steam_path):
+                print("The Civilization V folder was found.\n")
+                break
+            else:
+                print(f"The Civilization V folder was not fould at {steam_path}. Please retry or exit with '/exit'")
+
+    # list subscribed mods
+    mods = list_mods()
+
+    # copy mods
+    copy_mods(mods)
+
+    # saving pkg file
+    backup_pkg_file()
+
+    # writing to pkg file
+    write_pkg_file()
+
+    print("Install successful")
+    sleep(5)
